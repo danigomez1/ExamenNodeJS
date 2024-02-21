499ce8546abf0ba21a8b7328a6cf73e346fbda44
danielgomez1 <daniel.gomez@institutvidreres.cat>
2024-02-21 14:30:49 +0100

    Mètode put

--- src/danielGomezSanchez.js
+++ src/danielGomezSanchez.js
@@ -5,6 +5,28 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
+//paràmetres firebase
+var admin = require("firebase-admin");
+
+var serviceAccount = require("C:\\Users\\redho\\WebstormProjects\\ExamenNodeJS\\src\\exdanielgomezsanchez-firebase-adminsdk-mssy9-34eca29102.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+//mètode put
+app.put('danielGomez/jugadors', async (req, res) => {
+  const db = admin.firestore();
+  const estadi = db.collection('mundial22Gomez')
+  const lusail = {lusail: req.body.lusail}
+  await estadi.doc('estadi').update({
+    lusail: lusail
+  })
+})
+
+//mètode get
+
+
 app.listen(port, () => {
   console.log(`Servidor escoltant al port: ${port}`);
 })
